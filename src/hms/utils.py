fdec0064a3a9faa13341c7768d2d9694e4fffc43
Hui Li <herdingcat@yahoo.com>
2022-07-30 22:45:48 -0700

    add color plate rotation + rrd ds functions
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

--- src/hms/utils.py
+++ src/hms/utils.py
@@ -3,6 +3,7 @@
 import glob
 import os
 import re
+import rrdtool
 import yaml
 
 
@@ -16,6 +17,29 @@ def get_disk_devices():
         for dir in glob.glob("/sys/class/block/*")
         if not re.match(r"^loop|^zram|^sr", os.path.basename(dir))
     ]
+
+
+def get_rrd_ds(rrd_filename):
+    ds = []
+
+    rrd_info = rrdtool.info(rrd_filename)
+
+    for key in rrd_info:
+        if key.startswith("ds"):
+            device_name = re.search(r"^ds\[(.*)\].*", key).group(1)
+            if device_name not in ds:
+                ds.append(device_name)
+
+    return sorted(ds)
+
+
+def rotate_color_plate(items, color_plate):
+    final_color_plate = []
+
+    rotate_count, extra_color_count = divmod(len(items), len(color_plate))
+    final_color_plate = rotate_count * color_plate + color_plate[0:extra_color_count]
+
+    return final_color_plate
 
 
 def read_config(config_file):
